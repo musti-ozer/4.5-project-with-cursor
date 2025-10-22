41cc72390162f14de9d497223a51ce43168454ef
musti-ozer <musti_ozer@hotmail.com>
2025-10-23 01:41:16 +0300

    Add RESTful endpoint annotations to ProductsController for improved API functionality.

--- product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
+++ product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
@@ -2,6 +2,11 @@ package com.turkcell.product_service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/api/v1/products")
